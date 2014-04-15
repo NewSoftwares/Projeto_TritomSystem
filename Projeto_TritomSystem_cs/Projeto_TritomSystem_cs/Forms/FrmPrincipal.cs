b5d619825e2a8cd06d5a07442c3a2fce8b9f0c73
caioborges89 <caiocesarborges89@gmail.com>
2014-04-15 11:02:35 -0300

    Novo Projeto TritomSystem em C#

---
+++ Projeto_TritomSystem_cs/Projeto_TritomSystem_cs/Forms/FrmPrincipal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Projeto_TritomSystem_cs
+{
+    public partial class FrmPrincipal : Form
+    {
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmPrincipal_Load(object sender, EventArgs e)
+        {
+            this.Text = "TRITOM SYSTEM - VERSÃO " + Application.ProductVersion;
+        }
+    }
+}
